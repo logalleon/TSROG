c02637055be09af01cda0ab7517abec9754f37be
Logan Sobczak <lsobczak@352inc.com>
2018-05-14 11:21:11 -0400

    adds die rolling

--- src/Random/Dice.ts
+++ src/Random/Dice.ts
@@ -8,8 +8,17 @@ enum StandardDice {
   d20 = 'd20'
 }
 
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 const rollDice = (dice: string): number => {
-  return 1;
+  let roll = 0;
+  const [die, bonus] = dice.split('+');
+  const [iterations, range] = die.split('d');
+  console.log(die, bonus, iterations, range);
+  for (let i = 0; i < (iterations ? Number(iterations) : 1); i++) {
+    roll += randomInt(1, range);
+  }
+  return roll + (bonus ? Number(bonus) : 0);
 };
 
-export { rollDice, StandardDice };
+export { rollDice, StandardDice, randomInt };
